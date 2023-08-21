fcf309c5d47d68af3d5e695410116e397027128a
jillianguerra <guerrajilliant@gmail.com>
2023-08-21 18:42:13 -0400

    changes port

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,7 @@ gulp.task('default', (cb) => {
  // SERVE  FRONT END WITH PROXY TO BACKEND
 	browserSync.init({
 	 proxy: {
-		 target: 'http://localhost:8010',
+		 target: 'http://localhost:8020',
 		 ws: true
 	 },
 	 serveStatic: ['./public']
